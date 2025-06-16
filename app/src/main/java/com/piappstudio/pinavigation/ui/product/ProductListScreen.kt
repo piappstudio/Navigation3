a76b68174944aa117d7903c2d86c65d6681a3d0f
Pi App Studio <piappstudio@gmail.com>
2025-06-16 00:50:57 -0500

    Added autoSize

--- app/src/main/java/com/piappstudio/pinavigation/ui/product/ProductListScreen.kt
+++ app/src/main/java/com/piappstudio/pinavigation/ui/product/ProductListScreen.kt
@@ -2,17 +2,20 @@ package com.piappstudio.pinavigation.ui.product
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.TextAutoSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -35,27 +38,35 @@ fun ProductListScreen(
             Text(text = "Product List")
         })
     }) { innerPadding ->
-        LazyColumn(modifier = Modifier.padding(innerPadding)) {
-            items(arrOfList) { item ->
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                        .clickable {
+        Column(modifier = Modifier.padding(innerPadding)) {
+            LazyColumn(modifier = Modifier.padding(innerPadding)) {
+                item {
+                    Text("Product displayed here is more important ", modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                        maxLines = 1,
+                        autoSize = TextAutoSize.StepBased(minFontSize = MaterialTheme.typography.bodySmall.fontSize, maxFontSize = MaterialTheme.typography.headlineLarge.fontSize), style = MaterialTheme.typography.bodyLarge)
+                }
+                items(arrOfList) { item ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .clickable {
+                                backStack.add(ProductDetail(item.id ?: "Default"))
+                            }) {
+                        Text(item.name ?: "Default")
+                        IconButton(onClick = {
                             backStack.add(ProductDetail(item.id ?: "Default"))
                         }) {
-                    Text(item.name ?: "Default")
-                    IconButton(onClick = {
-                        backStack.add(ProductDetail(item.id ?: "Default"))
-                    }) {
-                        Icon(Icons.Default.ChevronRight, "Detail")
-                    }
+                            Icon(Icons.Default.ChevronRight, "Detail")
+                        }
 
+                    }
+                    HorizontalDivider()
                 }
-                HorizontalDivider()
             }
         }
+
     }
 }
